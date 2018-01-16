b22a5ab94c3c87c33ba64b23ff2d3325db858287
Peter <xutongle@gmail.com>
2018-01-16 12:35:12 +0800

    完善

--- components/Target.php
+++ components/Target.php
@@ -8,13 +8,105 @@
 namespace yuncms\notification\components;
 
 use yii\base\Component;
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
 
 abstract class Target extends Component
 {
     /**
+     * @var string target id.
+     */
+    private $_id;
+
+    /**
+     * @var string target name.
+     */
+    private $_name;
+
+    /**
+     * @var string target title to display in views.
+     */
+    private $_title;
+
+    /**
      * @var bool 是否启用此通知目标
      */
     private $_enabled = true;
+
+    /**
+     * @param string $id target id.
+     */
+    public function setId($id)
+    {
+        $this->_id = $id;
+    }
+
+    /**
+     * @return string target id
+     */
+    public function getId()
+    {
+        if (empty($this->_id)) {
+            $this->_id = $this->getName();
+        }
+        return $this->_id;
+    }
+
+    /**
+     * @param string $name target name.
+     */
+    public function setName($name)
+    {
+        $this->_name = $name;
+    }
+
+    /**
+     * @return string target name.
+     */
+    public function getName()
+    {
+        if ($this->_name === null) {
+            $this->_name = $this->defaultName();
+        }
+        return $this->_name;
+    }
+
+    /**
+     * @param string $title target title.
+     */
+    public function setTitle($title)
+    {
+        $this->_title = $title;
+    }
+
+    /**
+     * @return string target title.
+     */
+    public function getTitle()
+    {
+        if ($this->_title === null) {
+            $this->_title = $this->defaultTitle();
+        }
+        return $this->_title;
+    }
+
+    /**
+     * Generates target name.
+     * @return string target name.
+     */
+    protected function defaultName()
+    {
+        return Inflector::camel2id(StringHelper::basename(get_class($this)), '');
+    }
+
+    /**
+     * Generates target title.
+     * @return string target title.
+     */
+    protected function defaultTitle()
+    {
+        return StringHelper::basename(get_class($this));
+    }
 
     /**
      * Exports notification [[messages]] to a specific destination.
